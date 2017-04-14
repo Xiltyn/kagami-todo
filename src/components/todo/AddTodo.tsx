277f248f08ba5b9060dd74a5c2fc183fa08c6098
Xiltyn <xiltyn@gmail.com>
2017-04-14 13:11:50 +0200

    minor changes + graphql install

--- src/components/todo/AddTodo.tsx
+++ src/components/todo/AddTodo.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import StatusIcon from "./StatusIcon";
 
 export default class AddTodo extends React.Component<any, any> {
 	constructor() {
@@ -20,17 +19,7 @@ export default class AddTodo extends React.Component<any, any> {
 		return(
 			<div className={"addTodo " + isActive} onClick={this._toggleInput}>
 				<div className="box">
-					<input className="category" type="text"/>
-					<div className="todo-content">
-						<input type="text"/>
-						<input type="text"/>
-					</div>
-					<div className="priority">
-						<div className="neutral" />
-					</div>
-				</div>
-				<div className="footnotes">
-					<input type="text" />
+                    <div unselectable={true} className="addIcon">+</div>
 				</div>
 			</div>
 		)
